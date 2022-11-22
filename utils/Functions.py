ec879221f06b0d87d83be0213ac1fb6062029265
wuzhican <wuzhican1234@163.com>
2022-11-22 16:39:55 +0800

    debug:先clip后正则化

--- utils/Functions.py
+++ utils/Functions.py
@@ -50,8 +50,8 @@ def show_pil(img,title=None):
 def show_image(img,title=None):
     un = UnNormalize((0.229, 0.224, 0.225),(0.485, 0.456, 0.406))
     img = img.clone().detach().cpu()
-    img = un(img).data.numpy()
     img = img.transpose(1,2,0).clip(0,1)
+    img = un(img).data.numpy()
     print(title)
     plt.figure(title)
     plt.imshow(img)
