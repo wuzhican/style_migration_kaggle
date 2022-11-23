0a0457dadbe384f2eb409d9fa0d892d217349275
wuzhican <wuzhican1234@163.com>
2022-11-23 14:54:45 +0800

    debug:调整ImfwNet

--- models/ImfwNet.py
+++ models/ImfwNet.py
@@ -64,9 +64,9 @@ class ImfwNet(pl.LightningModule):
             nn.ConvTranspose2d(32, 3, 9, 1)
         ])
         self.model = MyModuleList([
-            nn.Conv2d(3,128,3),
-            nn.InstanceNorm2d(128, affine=True),
-            nn.ConvTranspose2d(128,3,3),
+            nn.Conv2d(3,128,1),
+            # nn.InstanceNorm2d(128, affine=True),
+            nn.ConvTranspose2d(128,3,1),
             nn.ReLU()
         ])
 
