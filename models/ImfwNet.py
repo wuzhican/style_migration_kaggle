241354c62be8a2fabda3f3af028c0ab6e933af61
wuzhican <wuzhican1234@163.com>
2022-11-23 15:50:37 +0800

    debug:

--- models/ImfwNet.py
+++ models/ImfwNet.py
@@ -66,7 +66,7 @@ class ImfwNet(pl.LightningModule):
         self.model = nn.Sequential(
             nn.Conv2d(3,128,1),
             nn.InstanceNorm2d(128, affine=True),
-            # nn.Conv2d(128,3,1),
+            nn.Conv2d(128,3,1),
             # nn.ConvTranspose2d(128,3,1),
             nn.ReLU()
         )
