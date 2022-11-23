d5d6bc0c09aa0547967027426d6761c6f2020a9e
wuzhican <wuzhican1234@163.com>
2022-11-23 15:10:56 +0800

    debug:移除relu

--- models/ImfwNet.py
+++ models/ImfwNet.py
@@ -68,7 +68,7 @@ class ImfwNet(pl.LightningModule):
             nn.InstanceNorm2d(128, affine=True),
             nn.Conv2d(128,3,1),
             # nn.ConvTranspose2d(128,3,1),
-            nn.ReLU()
+            # nn.ReLU()
         ])
 
     def forward(self, x):    
