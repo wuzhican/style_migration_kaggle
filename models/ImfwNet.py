785c7ffa0313fae38d5e94cbbcd42ea5b60c3b8c
wuzhican <wuzhican1234@163.com>
2022-11-23 09:30:21 +0800

    debug:去除修改设备的代码

--- models/ImfwNet.py
+++ models/ImfwNet.py
@@ -121,12 +121,12 @@ class FWNetModule(pl.LightningModule):
         return parent_parser
     
     def training_step(self, batch,batch_index):
-        if(str(self.device).find('cuda') != -1 and str(self.style.device) != str(self.device)):
-            self.style = self.style.to(self.device)
-            self.feature_net = self.feature_net.to(self.device)
-            self.fwNet = self.fwNet.to(self.device)
-            self.style_features = self.feature_net(self.style)
-            self.style_grams = {layer: gram_matrix(self.style_features[layer]) for layer in self.style_features}
+        # if(str(self.device).find('cuda') != -1 and str(self.style.device) != str(self.device)):
+        #     self.style = self.style.to(self.device)
+        #     self.feature_net = self.feature_net.to(self.device)
+        #     self.fwNet = self.fwNet.to(self.device)
+        #     self.style_features = self.feature_net(self.style)
+        #     self.style_grams = {layer: gram_matrix(self.style_features[layer]) for layer in self.style_features}
         opt = self.optimizers()
         opt.zero_grad()
         x = batch
