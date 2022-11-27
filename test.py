b7158ceb042aac2d916fa90aa62504eab2f8ccde
wuzhican <wuzhican1234@163.com>
2022-11-27 16:49:36 +0800

    feat:更新不计算tv loss的cyber punk风格

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ arg_v = vars(args)
 
 if __name__ == '__main__':
     if arg_v['model'] == 'CameraTester':
-        tester = getattr(tester,arg_v['model'])('./data/lightning_logs/ImfwNet/version_4/checkpoints/epoch=9-step=2590.ckpt')
+        tester = getattr(tester,arg_v['model'])('./data/lightning_logs/ImfwNet/version_5/checkpoints/epoch=14-step=3885.ckpt')
     else :
         tester = getattr(tester,arg_v['model'])()
     tester.run()
